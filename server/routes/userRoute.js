09f815c6b393c397fa7689568c5e4ec952b9d7fb
sobreeH <wansobree.hajihama@gmail.com>
2025-09-28 01:07:15 +0700

    #7 done

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,6 +4,7 @@ import {
   create,
   getAllUsers,
   getUserById,
+  update,
 } from "../controller/userController.js";
 
 const route = express.Router();
@@ -11,5 +12,6 @@ const route = express.Router();
 route.post("/user", create);
 route.get("/users", getAllUsers);
 route.get("/user/:id", getUserById);
+route.put("/update/user/:id", update);
 
 export default route;
